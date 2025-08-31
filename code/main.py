6194413b25dc6ff36f6c015f26a3ab9f8caeefd1
Joana Morais <joanaalvesm@gmail.com>
2025-08-31 20:13:18 -0300

    feat: implementa algoritmo

---
+++ code/main.py
@@ -0,0 +1,24 @@
+def maxmin_select(arr, left, right):
+    if left == right:
+        return arr[left], arr[left]  # (min, max)
+    
+    if right == left + 1:
+        if arr[left] < arr[right]:
+            return arr[left], arr[right]
+        else:
+            return arr[right], arr[left]
+    
+    mid = (left + right) // 2
+    
+    min1, max1 = maxmin_select(arr, left, mid)
+    min2, max2 = maxmin_select(arr, mid + 1, right)
+    
+    # Combinação dos resultados
+    return min(min1, min2), max(max1, max2)
+
+if __name__ == "__main__":
+    numeros = [7, 2, 9, 4, 1, 6, 10, 3, 152, 8, 5]
+    minimo, maximo = maxmin_select(numeros, 0, len(numeros) - 1)
+    print("Sequência:", numeros)
+    print("Menor elemento:", minimo)
+    print("Maior elemento:", maximo)
